02dc62b7247d06112bb9236d6f9bfe276cb5a7f5
Antony Itteera <antonyItteera@gmail.com>
2022-03-03 18:46:41 +0000

    included tap service

--- FleetManagement/src/main/java/com/tus/EasyFare/FleetManagement/Bus/dao/BusRepository.java
+++ FleetManagement/src/main/java/com/tus/EasyFare/FleetManagement/Bus/dao/BusRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.tus.EasyFare.FleetManagement.Bus.Entity.Bus;
 
 public interface BusRepository extends JpaRepository<Bus, Long> {
-
+	Bus findDistinctByBusNumberAndRouteNumber(long busNumber,String routeNumber);
 }
